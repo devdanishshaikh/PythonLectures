3b885c597f29c76a8fc5cbb46bcb2cc401aa9ea0
devdanishshaikh <shaikhdani710@gmail.com>
2026-02-24 21:16:14 +0500

    feat(functions): Add parameterized functions, return values & average calculator
    - Added function with parameters and return value (math sum example)
    - Added exercise: function to take 3 inputs and return their average
    - Organized previous examples (greet, add, factorial) with comments

--- function.py
+++ function.py
@@ -1,16 +1,33 @@
-def greet():
+'''def greet():
   print("Hello, World!")
-greet()
+greet()'''
 
 
-def add(a, b):
+'''def add(a, b):
   return a + b
 result = add(5, 3)
-print(result)
+print(result)'''
 
-def factorial(n):
+'''def factorial(n):
   if n == 0:
     return 1
   else:
     return n * factorial(n - 1)
-print(factorial(5))
+print(factorial(5))'''
+
+#function using parameters and return value
+'''def math(a,b):
+  go=a+b
+  return go
+result=math(10,20)
+print("sum :",result)'''
+
+
+#1. make a function which take 3 input value and gives their average as output
+'''def average(a,b,c):
+  return (a+b+c)/3
+a=input("Enter first number: ")
+b=input("Enter second number: ")
+c=input("Enter third number: ")
+
+print(average(int(a),int(b),int(c)))'''
