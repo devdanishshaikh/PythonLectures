21853c76873bd9516c1d64b4bb7be5f83dbe0091
devdanishshaikh <shaikhdani710@gmail.com>
2026-02-25 22:53:37 +0500

    Add string.py

---
+++ string.py
@@ -0,0 +1,20 @@
+'''name="Danish a"
+print("Name :",name)
+print("Type of name:",type(name))
+print("Length of name:",len(name))
+print(name.replace("a","A"))
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+print(name[6])
+print(name[7])
+print(name[8])
+for ch in name:
+    print(ch,end=" ")
+'''
+word="I am a student at Mehran University of Engineering and Technology"
+print(word)
+print(word[8:-4])
