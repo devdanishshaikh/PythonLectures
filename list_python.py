c5b2bfab2e0f53a204aa2acfc7e3275068f4c005
devdanishshaikh <shaikhdani710@gmail.com>
2026-02-25 23:10:57 +0500

    Update list_python.py

--- list_python.py
+++ list_python.py
@@ -1,1 +1,8 @@
-marks =[10,99,23,45,67,89,90]
+'''marks =[10,99,23,45,67,89,90]
+marks.append(100)
+marks.insert(2, 50)
+marks.remove(45)
+marks.pop(0)
+marks.reverse()
+print(len(marks))
+print(marks[2:6])'''
