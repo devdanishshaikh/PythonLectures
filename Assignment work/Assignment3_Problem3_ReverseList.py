95328b2c3abe969be0e1e37fb83f01757bf8e159
devdanishshaikh <shaikhdani710@gmail.com>
2026-03-14 07:27:18 +0500

    Moved all Python files from root to 'work' folder and cleaned up assignments

--- Assignment work/Assignment3_Problem3_ReverseList.py
+++
@@ -1,8 +0,0 @@
-# ==================================
-# List Example - Problem 3
-# ==================================
-# Reverse a list
-
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-reverse_list = nums[::-1]
-print("Reversed list:", reverse_list)
